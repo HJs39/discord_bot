dee37f121f2390f23d7060aa077ed755089670b1
H_js <h.jason1231@icloud.com>
2026-08-27 02:42:29 +0800

    add new command to list persona for user

---
+++ slash_commands/list_persona.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder, MessageFlags } = require('discord.js');
+const { client } = require('../assets/client.js');
+
+module.exports = {
+    command: new SlashCommandBuilder()
+        .setName('list_persona')
+        .setDescription('list existed personas')
+        .addBooleanOption(option => option.setName('ephemeral')
+            .setDescription('select the list would be ephemeral or not(defualt is false)')),
+    eval: async function (interaction) {
+        /**@type {import('../implement/LLM/persona_manager.js').filtered_persona_t[]} */
+        const personas = client.battle.get_list_user_seeable(interaction.user.id);
+        const ephemeral = interaction.options.getBoolean('ephemeral') ?? false;
+        await interaction.deferReply({ flags: ephemeral ? MessageFlags.Ephemeral : undefined });
+        let output = '```';
+        personas.forEach((persona) => {
+            output += `[${persona.id}|${persona.persona.type}]: ${persona.persona.display_name}${persona.persona.deprecated ? '(deprecated)' : ''}\n`;
+        });
+        await interaction.editReply(output.trimEnd() + '```');
+    }
+}
