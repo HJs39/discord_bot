5a8b4f5afc0736bbcc899b35ef391128cca0dea7
H_js <h.jason1231@icloud.com>
2026-08-27 11:44:52 +0800

    use member?.displayName first

--- slash_commands/say.js
+++ slash_commands/say.js
@@ -1,24 +1,37 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
     command: new SlashCommandBuilder()
         .setName('say')
         .setDescription('let Alice say something you want')
         .addStringOption(option => option.setName('message')
-            .setDescription('the content you want Alice to say')
-            .setRequired(true))
+            .setDescription('the content you want Alice to say'))
         .addAttachmentOption(option => option.setName("attachment")
             .setDescription("the attachment you want Alice to send")),
     eval: async function (interaction) {
-        if (interaction.options.getAttachment('attachment') !== null) {
-            await interaction.reply({
-                content: `${interaction.user.globalName || interaction.user.userName}想說:\n${interaction.options.getString('message')}`,
-                files: [interaction.options.getAttachment('attachment')]
-            });
+        const display_name = interaction.member?.displayName ?? interaction.user.displayName;
+        if (interaction.options.getString('message') !== null) {
+            if (interaction.options.getAttachment('attachment') !== null) {
+                await interaction.reply({
+                    content: `${display_name}想說:\n${interaction.options.getString('message')}`,
+                    files: [interaction.options.getAttachment('attachment')]
+                });
+            } else {
+                await interaction.reply({
+                    content: `${display_name}想說:\n${interaction.options.getString('message')}`
+                });
+            }
         } else {
-            await interaction.reply({
-                content: `${interaction.user.globalName || interaction.user.userName}想說:\n${interaction.options.getString('message')}`
-            });
+            if (interaction.options.getAttachment('attachment') !== null) {
+                await interaction.reply({
+                    files: [interaction.options.getAttachment('attachment')]
+                });
+            } else {
+                await interaction.reply({
+                    content: `沒有想說的話就不要用啦~`,
+                    flags: MessageFlags.Ephemeral
+                });
+            }
         }
     }
 };
