93e732540f7c5087a07fe3cb97fe2168bb5341ee
H_js <h.jason1231@icloud.com>
2026-08-30 01:28:41 +0800

    add chat API config

--- assets/client.js
+++ assets/client.js
@@ -3,14 +3,23 @@ const LLM_interface = require('../implement/LLM/LLM_interface');
 const cooldown = require('./cooldown.json');
 const bot_assets = require('./bot_assets.json');
 const battle_API_config = require('./private-battle_API_config.json');
+const chat_API_config = require('./private-chat_API_config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
 /**@type {Collection} */
 client.commands = new Collection();
 client.cooldown = new Collection();
 for (const func of cooldown) {
     client.cooldown.set(func, new Collection());
 }
+/**@type {LLM_interface} */
+client.chat = new LLM_interface(chat_API_config, false, false);
 /**@type {LLM_interface} */
 client.battle = new LLM_interface(battle_API_config, false, false);
 client.is_owner = (id) => { return id === bot_assets.owner; };
