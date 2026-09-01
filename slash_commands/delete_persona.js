04043a35aa4e41199a8adce999a87553c7a5cff2
H_js <h.jason1231@icloud.com>
2026-09-01 22:26:18 +0800

    simplify implement

--- slash_commands/delete_persona.js
+++ slash_commands/delete_persona.js
@@ -10,16 +10,20 @@ module.exports = {
             .setRequired(true)
             .setAutocomplete(true)),
     eval: async function (interaction) {
-        /**@type {import('../implement/LLM/persona_manager.js').filtered_persona_t[]} */
-        const list = client.battle.get_persona_list_by_author(interaction.user.id);
         /**@type {number} */
         const persona_id = interaction.options.getInteger('persona');
-        /**@type {import('../implement/LLM/persona_manager.js').filtered_persona_t} */
-        const persona = list.find(p => p.id === persona_id);
-        if (persona) {
-            client.battle.deprecated_persona(persona_id);
+        /**@type {persona} */
+        const persona = client.chat.get_persona(persona_id);
+        if (!persona) {
             await interaction.reply({
-                content: `${persona.persona.display_name}現在處於棄用狀態啦！`,
+                content: `這個設定不存在！`,
+                flags: MessageFlags.Ephemeral
+            });
+            return;
+        } else if (persona.author !== interaction.user.id) {
+            client.chat.deprecated_persona(persona_id);
+            await interaction.reply({
+                content: `${persona.display_name}現在處於棄用狀態啦！`,
                 flags: MessageFlags.Ephemeral
             });
             return;
@@ -34,7 +38,7 @@ module.exports = {
     complete: async function (interaction) {
         const focus = interaction.options.getFocused();
         /**@type {import('../implement/LLM/persona_manager.js').filtered_persona_t[]} */
-        const user_created = client.battle.get_persona_list_by_author(interaction.user.id).filter(p => !p.persona.deprecated);
+        const user_created = client.chat.get_persona_list_by_author(interaction.user.id).filter(p => !p.persona.deprecated);
         const idx = parseInt(focus);
         if (isNaN(idx)) {
             await interaction.respond(user_created.filter(p => p.persona.display_name.startsWith(focus)).map(p => ({ name: p.persona.display_name, value: p.id })));
